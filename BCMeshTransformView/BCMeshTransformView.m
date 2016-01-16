d286ff3fac44232c3d15323889f1e2f531e846eb
Dave Peck <peckda@gmail.com>
2016-01-15 16:26:19 -0800

    Disable render when hidden.

--- BCMeshTransformView/BCMeshTransformView.m
+++ BCMeshTransformView/BCMeshTransformView.m
@@ -192,10 +192,12 @@
 
 - (void)setNeedsContentRendering
 {
+    if (self.hidden) return;
     if (self.pendingContentRendering == NO) {
         // next run loop tick
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0)), dispatch_get_main_queue(), ^{
             //NSLog(@"[%li] PERFORMING RENDER", self.tag);
+            if (self.hidden) return;
             [self.texture renderView:self.contentView];
             [self.glkView setNeedsDisplay];
             self.pendingContentRendering = NO;
